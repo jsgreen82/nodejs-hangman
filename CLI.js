78fd0dd6051963b7d50465f6876456920e8500cb
jsgreen82 <jacquegreen@gmail.com>
2017-12-08 15:40:03 -0700

    mostly completed files

---
+++ CLI.js
@@ -0,0 +1,160 @@
+var inquirer = require('inquirer');
+var guessWordList = require('./game.js');
+var checkForLetter = require('./words.js');
+var lettersToDisplay = require('./letters.js');
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var lettersAlreadyGuessed = [];
+var lettersCorrectlyGuessed = [];      
+var displayHangman;
+
+var game = {
+
+  wordBank : guessWordList, // import a list of words
+  guessesRemaining : 10, // per word
+  currentWrd : null, // the word object
+
+
+  startGame : function(){
+    // make sure the user has 10 guesses
+    this.guessesRemaining = 10;
+
+    // get a random word from the array
+    var j = Math.floor(Math.random() * this.wordBank.length);
+    this.currentWrd = this.wordBank[j];
+
+    // Inform User game has begun
+    console.log('Guess the 20th Century Artist');
+
+    // Show the empty letters ( _ _ _ _ ) and guesses, etc.
+    displayHangman = new lettersToDisplay(this.currentWrd);
+    displayHangman.parseDisplay();
+    console.log('Guesses Left: ' + game.guessesRemaining);
+
+    // prompt for a letter
+    keepPromptingUser();
+  }
+
+};
+
+// User Prompt Function (stand alone b/c of scoping issues inside the game object)
+
+function keepPromptingUser(){
+
+  // Always make a gap between inputs
+  console.log('');
+
+  // If enough guesses left, then prompt for new letter
+  if(game.guessesRemaining > 0){
+    inquirer.prompt([
+      {
+        type: "value",
+        name: "letter",
+        message: "Guess a Letter: "
+      }
+    ]).then(function(userInput){
+
+      // Collect Letter Input
+      var inputLetter = userInput.letter.toLowerCase();
+      
+      // Valid input
+      if(alphabet.indexOf(inputLetter) == -1){
+
+        // Tell user they did not guess a letter
+        console.log('Guess again, "' + inputLetter + '" is not a letter.');
+        console.log('Guesses Left: ' + game.guessesRemaining);
+        console.log('Letters already guessed: ' + lettersAlreadyGuessed);
+        keepPromptingUser();
+
+      }
+      else if(alphabet.indexOf(inputLetter) != -1 && lettersAlreadyGuessed.indexOf(inputLetter) != -1){
+
+        // Tell user they already guessed that letter
+        console.log('You already guessed "' + inputLetter + '". Try again!');
+        console.log('Guesses Left: ' + game.guessesRemaining);
+        console.log('Letters already guessed: ' + lettersAlreadyGuessed);
+        keepPromptingUser();
+
+      }
+      else{
+
+        // Remove the entry from the list of possible inputs
+        lettersAlreadyGuessed.push(inputLetter);
+
+
+        // Check for the letter in the word
+        var letterInWord = checkForLetter(inputLetter, game.currentWrd);
+
+        // If the letter is in the word, update the letter object
+        if(letterInWord){
+
+          // Add to correct letters list
+          lettersCorrectlyGuessed.push(inputLetter);
+
+          // Show the empty letters ( _ _ _ _ ) and guesses, etc.
+          displayHangman = new lettersToDisplay(game.currentWrd, lettersCorrectlyGuessed);
+          displayHangman.parseDisplay();
+
+
+          // Test if the user has won
+          if(displayHangman.winner){
+            console.log('You win!');
+            return;
+          }
+          // Not a win yet, so ask for another input and decrement guesses
+          else{
+            console.log('Guesses Left: ' + game.guessesRemaining);
+            console.log('Letters already guessed: ' + lettersAlreadyGuessed);
+            keepPromptingUser();
+          }
+
+        }
+        // Otherwise, decrement guesses and re-prompt the old hangman object
+        else{
+          game.guessesRemaining--;
+
+          displayHangman.parseDisplay();
+          console.log('Guesses Left: ' + game.guessesRemaining);
+          console.log('Letters already guessed: ' + lettersAlreadyGuessed);
+          keepPromptingUser();
+        }
+        
+      }
+
+    });
+    
+  }
+  // If not enough guesses left, then user losses
+  else{
+   
+    console.log('Out of guesses. You lose.');
+    console.log('The artist was "' + game.currentWrd + '".');
+  }
+
+}
+
+
+// Create a new game object using the constructor and begin playing
+game.startGame();
+
+
+//replay function
+// var replay = function() {
+// inquirer.prompt([
+// 	name: "play",
+// 	message: "Play Again? (y/n)"
+// ])
+
+// .then(function(answer) {
+// 	var confirm = answer.play.toLowerCase();
+// 	if(confirm === "y") {
+// 		guessesRemaining = 0;
+		
+// 	} else (confirm == 'n'){
+// 			console.log('Thanks for playing!');
+			
+// 	} 
+    
+// })
+
+
+// };
---
+++ game.js
@@ -0,0 +1,4 @@
+var listOfWords = ["Matisse", "Picasso", "Duchamp", "Kandinsky", "Rothko", "Pollock", "Warhol", "Lichtenstein", "Klee", "Kahlo", "Dali", "Cezanne", "Brancusi", "Johns", "Hopper", "Sherman", "Ray", "Braque", "Rauschenberg", "Harper"];
+
+// Export the list of words to other hangman script(s)
+module.exports = listOfWords;
